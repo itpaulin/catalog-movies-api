22fab12abb69f339d7bc0f821cc2f80533281f97
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-11 15:49:12 -0300

    feat: add pagination

--- src/infra/http/controllers/movie.controller.ts
+++ src/infra/http/controllers/movie.controller.ts
@@ -1,24 +1,41 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Query } from '@nestjs/common';
 import { MovieService } from '../../../app/movie.service';
-import { Movie as MovieModel } from '@prisma/client';
+import { Movie, Movie as MovieModel } from '@prisma/client';
 @Controller('movies')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get()
-  async getMovies(): Promise<MovieModel[]> {
-    const movies = await this.movieService.movies({});
+  async getMovies(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ): Promise<{ data: Movie[]; total: number }> {
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
 
-    const sortedMovies = movies.sort((a, b) =>
-      a.releaseDate > b.releaseDate ? 1 : -1,
-    );
+    const movies = await this.movieService.movies({
+      page: pageNumber,
+      limit: limitNumber,
+      orderBy: { releaseDate: 'desc' },
+    });
+    const total = await this.movieService.countMovies();
 
-    return sortedMovies;
+    return {
+      data: movies,
+      total,
+    };
   }
 
   @Get('update')
-  async updateListMovies(): Promise<MovieModel[]> {
+  async updateListMovies(): Promise<string> {
     await this.movieService.deleteAllMovies();
-    return this.getMovies();
+
+    const moviesFromAPI = await this.movieService.fetchMoviesFromAPI();
+
+    for (const movie of moviesFromAPI) {
+      await this.movieService.createMovie(movie);
+    }
+
+    return 'Movies updated successfully';
   }
 }
