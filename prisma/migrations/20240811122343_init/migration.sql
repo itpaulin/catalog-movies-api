f1d8a75bcd0138696378e7ca4a227ec6ab8c5f28
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-11 11:12:22 -0300

    feat: config prisma and db

---
+++ prisma/migrations/20240811122343_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "imagePath" TEXT NOT NULL,
+    "releaseDate" TIMESTAMP(3) NOT NULL,
+    "overview" TEXT NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
