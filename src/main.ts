aaf1cb912c88d726648a16466de309a95d82635b
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-11 15:48:54 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Permite apenas solicitações do frontend local
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    credentials: true, // Permite o envio de cookies e cabeçalhos de autorização
+  });
+
   await app.listen(3000);
 }
 bootstrap();
