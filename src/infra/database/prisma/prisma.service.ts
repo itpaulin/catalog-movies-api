73115ce86b139fad130b92e05ed7e63e3e31f97e
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-12 20:55:33 -0300

    feat: ensure garantee has a indexState

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -6,6 +6,13 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
     try {
+      const hasData = await this.indexState.findFirst({});
+      if (!hasData) {
+        await this.indexState.create({
+          data: { page: 1 },
+        });
+      }
+
       await this.indexState.update({
         where: { id: 1 },
         data: { page: 1 },
