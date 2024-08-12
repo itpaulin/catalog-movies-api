925a5827fdacfb12ad78d3e59a8b795c816b16fd
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-12 20:24:38 -0300

    feat: ensure when app init page index is always 1 and delete movies

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -5,5 +5,15 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    try {
+      await this.indexState.update({
+        where: { id: 1 },
+        data: { page: 1 },
+      });
+
+      await this.movie.deleteMany({});
+    } catch (error) {
+      console.error('Erro ao atualizar o indexState ou deletar filmes:', error);
+    }
   }
 }
