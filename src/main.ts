2d5e073ad75f7da8fc30394f52d54f62b5a901b6
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-12 20:08:18 -0300

    feat: add postman to cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Permite apenas solicitações do frontend local
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-    credentials: true, // Permite o envio de cookies e cabeçalhos de autorização
+    origin: [
+      'http://localhost:5173',
+      'http://localhost',
+      'http://127.0.0.1',
+      'http://127.0.0.1:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(3000);
