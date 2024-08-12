a9cd4793992a09319eeecca69e18bd102c4a3534
Paulo Ricardo <prmaltaleal@gmail.com>
2024-08-12 20:08:37 -0300

    feat: inject index state service

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MovieController } from './controllers/movie.controller';
 import { MovieService } from 'src/app/movie.service';
 import { PrismaService } from '../database/prisma/prisma.service';
+import { IndexStateService } from 'src/app/index-state.service';
 
 @Module({
   controllers: [MovieController],
-  providers: [MovieService, PrismaService],
+  providers: [MovieService, PrismaService, IndexStateService],
 })
 export class HttpModule {}
